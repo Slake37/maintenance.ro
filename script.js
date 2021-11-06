6d61ce463a54d487e334293f663c55d2f0ebfcbe
Slake37 <moldovan.alin160@yahoo.co.uk>
2021-11-06 21:43:56 +0000

    lll

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let hamMenu = document.querySelector('.ham-menu');
 let navLinks = document.querySelector('.nav-links');
+let modal = document.querySelector('.modal');
+let privacy = document.querySelector('#privacy');
+let closeBtn = document.querySelector('.closeBtn');
+
+
 
 hamMenu.addEventListener('click', function(){
     navLinks.classList.toggle('open');
+})
+
+privacy.addEventListener('click', function(){
+    modal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click',function(){
+    modal.style.display = 'none';
 })
