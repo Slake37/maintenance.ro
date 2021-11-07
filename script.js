9afaa869cefe75ff35b0337141e24701462e2f46
Slake37 <moldovan.alin160@yahoo.co.uk>
2021-11-07 08:30:12 +0000

    lll

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let navLinks = document.querySelector('.nav-links');
 let modal = document.querySelector('.modal');
 let privacy = document.querySelector('#privacy');
 let closeBtn = document.querySelector('.closeBtn');
+let navLinksLi = document.querySelectorAll('.nav-link')
 
+
+navLinksLi.forEach(navLinkLi => {
+    navLinkLi.addEventListener('click', function(){
+        navLinks.classList.remove('open');
+    })
+});
 
 hamMenu.addEventListener('click', function(){
     navLinks.classList.toggle('open');
